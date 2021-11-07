079ecbcf09da61f926d0125f36b65c7dc4a78367
c-kip <callumkipin@gmail.com>
2021-11-07 00:20:18 -0400

    Added simple three.js testing code

--- functionality/three-rendering.js
+++ functionality/three-rendering.js
@@ -19,6 +19,15 @@ function main() {
     // Create a scene to render
     const scene = new THREE.Scene();
 
+    // Create a light
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
+    }
+
     // Create the geometry for a cube (box)
     const cubeWidth = 1;
     const cubeHeight = 1;
@@ -26,16 +35,48 @@ function main() {
     const geometry = new THREE.BoxGeometry(cubeWidth, cubeHeight, cubeDepth);
 
     // Create a material for the cube
-    const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+    const material = new THREE.MeshPhongMaterial({color: 0x44aa88});
 
     // Create a mesh for the cube (contains the geometry -> vertexes, material -> texture,
     // and position) and add it to the scene
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    const cubes = [
+        makeInstance(geometry, 0x44aa88,  0),
+        makeInstance(geometry, 0x8844aa, -2),
+        makeInstance(geometry, 0xaa8844,  2),
+    ];
 
     // Render the scene made with our camera
     renderer.render(scene, camera);
     console.log("Finished three-rendering.js");
+
+    // Update (render) the canvas
+    function render(time) {
+        time *= 0.001;  // convert time to seconds
+        
+        // Update the rotation of each cube
+        cubes.forEach((cube, ndx) => {
+            const speed = 1 + ndx * .1;
+            const rot = time * speed;
+            cube.rotation.x = rot;
+            cube.rotation.y = rot;
+        });
+        
+        // Render the scene, and request the browser calls this
+        // function to update the page constantly
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
+
+    // Creates an instance of the geometry with the given colour,
+    // and spawns it at position x
+    function makeInstance(geometry, color, x) {
+        const material = new THREE.MeshPhongMaterial({color});
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+        cube.position.x = x;
+        return cube;
+    } //end of makeInstance()
+    requestAnimationFrame(render);
 } //end of main()
 
 main();
